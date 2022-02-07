c5160b315a8cd6dd016f814ba708cd9cda1cf74c
miltonlundte19 <milton.lund@elev.ga.ntig.se>
2022-02-07 09:32:29 +0100

    grafiska delen av vieun

--- src/vieu/kryptervieu.java
+++ src/vieu/kryptervieu.java
@@ -4,4 +4,14 @@ import javax.swing.*;
 
 public class kryptervieu {
     private JPanel panel1;
+    private JTextArea mesigeInput;
+    private JTextArea keyinput;
+    private JButton filechoserBotton;
+    private JComboBox sparningsValjare;
+    private JLabel mesigeText;
+    private JButton borjaKryptering;
+    private JLabel kyetext;
+    private JLabel fileDisplayText;
+    private JLabel fellMedelande;
+    private JLabel sparningText;
 }
